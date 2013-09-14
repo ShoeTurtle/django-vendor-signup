8e3bddde5b16a7e87e3830730a3c177339839b00
Binay Budhathoki <binay_budhathoki@hotmail.com>
2013-09-14 12:13:01 +0530

    create session to store vendor_id

--- vendor-signup-master/vendor_signup/views.py
+++ vendor-signup-master/vendor_signup/views.py
@@ -18,6 +18,8 @@ def register(request, vendor_id):
 	#1. Query the vendor name from the vendor id
 	#2. Create Context and pass it over to the view
 	
+	#Saving the vendor_id into a session variable
+	request.session['vendor_id'] = vendor_id
 	vendorObj = get_object_or_404(Vendor, pk=vendor_id)
 
 	template = loader.get_template('home.html')
@@ -33,9 +35,10 @@ def register(request, vendor_id):
 def saveproject(request):
 
 	project_data = json.loads(request.GET.get('project_data'))	
+	v_id = request.session['vendor_id']
 
 	for val in project_data:
-		Project(vendor_id='1', project_title=val['title'], project_url=val['url']).save()
+		Project(vendor_id=v_id, project_title=val['title'], project_url=val['url']).save()
 			
 	return HttpResponse("OK")
 
